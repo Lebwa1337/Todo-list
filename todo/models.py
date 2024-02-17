da8bc4852a8400cd51af6aaffb090f8951e0ae53
LeBwa <lebwa1337@gmail.com>
2024-02-17 15:11:10 +0200

    Create models such as Task and Tag. Run migrations

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Task(models.Model):
+    content = models.TextField()
+    datetime = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(null=True, blank=True)
+    status = models.BooleanField(default=False)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name='tasks')
+
