0a9870b0654765539967ddcc33e9536a488f9c52
LeBwa <lebwa1337@gmail.com>
2024-02-17 16:20:02 +0200

    Create Tag list view and creation functionality

--- todo/urls.py
+++ todo/urls.py
@@ -5,6 +5,8 @@ from todo import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("create/", views.TaskCreateView.as_view(), name="task-create"),
+    path("tags/", views.TagListView.as_view(), name="tag-list"),
+    path("tags/create/", views.TagCreateView.as_view(), name="tag-create")
 ]
 
 app_name = "todo"
--- todo/views.py
+++ todo/views.py
@@ -1,9 +1,9 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
-from django.views.generic import CreateView
+from django.views import generic
 
 from todo.forms import TaskForm
-from todo.models import Task
+from todo.models import Task, Tag
 
 
 def index(request: HttpRequest) -> HttpResponse:
@@ -11,7 +11,19 @@ def index(request: HttpRequest) -> HttpResponse:
     return render(request, 'todo/index.html')
 
 
-class TaskCreateView(CreateView):
+class TaskCreateView(generic.CreateView):
     model = Task
     template_name = 'todo/task_form.html'
     form_class = TaskForm
+
+
+class TagListView(generic.ListView):
+    model = Tag
+    template_name = 'todo/tag_list.html'
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    template_name = 'todo/tag_form.html'
+    fields = "__all__"
+    success_url = "tags/"
